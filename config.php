887de152cf2fe19e325511057355bc9956c56026
Bruno Henrique <brunohdev@gmail.com>
2015-04-03 05:11:24 -0300

    Retirando comentários do config.php

--- config.php
+++ config.php
@@ -1,12 +1,5 @@
 <?php
 
-//try {
-//    $conexao = new \PDO("mysql:host=localhost;dbname=phpoojeitocerto","root","");
-//}
-//catch(\PDOException $e) {
-//    echo "Erro ao conectar: ".$e->getMessage();
-//}
-
 return array(
     'host' => 'localhost',
     'name' => 'pdo',
